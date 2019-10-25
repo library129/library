3ffb4e2ebe7710dfe956a877e5ef7fc57d03bb94
MelnikovAleksei <melnikov.aleksey.alexeyevich@gmail.com>
2019-10-25 14:06:56 +0300

    css

---
+++ main.js
@@ -0,0 +1,13 @@
+(function() {
+  const headings = document.querySelectorAll('h2.collapsible-headline');
+  Array.prototype.forEach.call(headings, heading => {
+    let btn = heading.querySelector('button');
+    let target = heading.nextElementSibling;
+    btn.onclick = () => {
+      let expanded =
+        btn.getAttribute('aria-expanded') === 'true' || false;
+      btn.setAttribute('aria-expanded', !expanded);
+      target.hidden = expanded;
+    }
+  })
+})()
