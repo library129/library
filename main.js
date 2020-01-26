9615ae3912b00273224993d0543b972b0affcb57
library129 <melnikov.aleksei.alexeyevich@gmail.com>
2020-01-26 14:16:29 +0300

    added auto theme color

--- main.js
+++ main.js
@@ -1,36 +1,62 @@
 (function() {
-  const headings = document.querySelectorAll('h2.collapsible-headline');
-  Array.prototype.forEach.call(headings, heading => {
-    let btn = heading.querySelector('button');
-    let target = heading.nextElementSibling;
-    btn.onclick = () => {
-      let expanded =
-        btn.getAttribute('aria-expanded') === 'true' || false;
-      btn.setAttribute('aria-expanded', !expanded);
-      target.hidden = expanded;
-    }
-  })
+	const headings = document.querySelectorAll('h2.collapsible-headline');
+	Array.prototype.forEach.call(headings, heading => {
+		let btn = heading.querySelector('button');
+		let target = heading.nextElementSibling;
+		btn.onclick = () => {
+			let expanded =
+				btn.getAttribute('aria-expanded') === 'true' || false;
+			btn.setAttribute('aria-expanded', !expanded);
+			target.hidden = expanded;
+		}
+	})
 })()
 function showHideMenu(id) {
-        let x = document.getElementById(id);
-        if (x.style.display === 'none') {
-          x.style.display = 'block';
-        } else {
-          x.style.display = 'none';
-        }
-      }
+				let x = document.getElementById(id);
+				if (x.style.display === 'none') {
+					x.style.display = 'block';
+				} else {
+					x.style.display = 'none';
+				}
+			}
 function day(id) {
-  let d = new Date();
-  let weekday = new Array(7);
-        weekday[0] = "Сегодня библиотека работает <br> с <b>12:00 до 20:00";
-        weekday[1] = "Сегодня библиотека не работает";
-        weekday[2] = "Сегодня библиотека работает <br> с 10:00 до 22:00. <br> Последний вторник месяца - санитарный день <br> (только прием книг и занятия в студиях)";
-        weekday[3] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
-        weekday[4] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
-        weekday[5] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
-        weekday[6] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
+	let d = new Date();
+	let weekday = new Array(7);
+				weekday[0] = "Сегодня библиотека работает <br> с <b>12:00 до 20:00";
+				weekday[1] = "Сегодня библиотека не работает";
+				weekday[2] = "Сегодня библиотека работает <br> с 10:00 до 22:00. <br> Последний вторник месяца - санитарный день <br> (только прием книг и занятия в студиях)";
+				weekday[3] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
+				weekday[4] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
+				weekday[5] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
+				weekday[6] = "Сегодня библиотека работает <br> с 10:00 до 22:00";
 
-  let n = weekday[d.getDay()];
-  document.getElementById(id).innerHTML = n;
+	let n = weekday[d.getDay()];
+	document.getElementById(id).innerHTML = n;
 }
-day("day");
+const menu = document.querySelector('.menu__box');
+const html = document.querySelector('html');
+const theme = document.querySelector('.theme');
+let date = new Date();
+let hour = date.getHours();
+
+function updateTheme(bgColor, textColor) {
+		html.style.backgroundColor = bgColor;
+		html.style.color = textColor;
+}
+
+function themeWithGetDate() {
+		if (hour >= 17 || hour <= 8) {
+			updateTheme('black', 'white')
+			theme.textContent = 'Включена темная тема';
+			theme.style.color = '#f52525';
+			theme.style.borderColor = '#f52525';
+		} else {
+			updateTheme('white', 'black');
+			theme.style.display = 'none';
+		}
+
+	}
+
+
+themeWithGetDate()
+day('day')
