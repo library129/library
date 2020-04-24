3e9f8a82e340407387e27f5a33a8e5c4bf628f11
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-04-24 14:41:47 +0300

    rewrite nav in html css

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,39 +13,12 @@
 	})()
 
 let body = document.querySelector('body');
-let nav = document.querySelector('nav');
-let menuBtn = document.querySelector('.menu__btn');
-let menuItem = document.querySelector('.menu__list');
+
 let searchInput = document.querySelector('input');
 let searchButton = document.querySelector('.search__btn');
-let checked = false;
 
-function changeMenu() {
-	let windowWidth = window.innerWidth;
-	if (windowWidth > 900) {
-		nav.setAttribute('class', 'desktop-nav');
-		menuBtn.style.display = 'none';
-		menuItem.style.display = 'flex';
-	} else {
-		nav.removeAttribute('class', 'desktop-nav');
-		menuBtn.style.display = 'block';
-		menuItem.style.display = 'none';
-	} 
-}
 
-function checkedMenu() {
-	if (!checked) {
-		menuItem.style.display = 'flex';
-		menuBtn.style.backgroundColor = 'white';
-		menuBtn.style.color = 'black';
-		checked = true;
-	} else {
-		menuItem.style.display = 'none';
-		menuBtn.style.backgroundColor = 'rgb(230, 183, 64)';
-		menuBtn.style.color = 'white';
-		checked = false;
-	}  
-}
+
 
 function searchBooks() {
 	fetch("./resources/catalog/catalog.json")
@@ -67,11 +40,4 @@ function searchBooks() {
 }
 
 searchButton.addEventListener('click', searchBooks)
-menuBtn.addEventListener('click', checkedMenu);
-window.addEventListener('resize', changeMenu);
-document.onload = changeMenu()
 
-function covid() {
-alert('Дорогие читатели! С 25 марта библиотеки Юго-Востока временно закрыты*. Эта мера связана с противодействием распространению на территории Москвы новой коронавирусной инфекции. Библиотеки ЮВАО остаются на связи: на сайте cbsuvao.ru и в социальных сетях публикуются вебинары, лекции, мастер-классы и многие другие активности, которые не дадут скучать в самоизоляции. Также просим связаться с библиотекой для уточнения продления сроков возврата книг. Берегите себя и своих близких, оставайтесь дома. *Во исполнение указа Мэра Москвы от 16 марта 2020 г. №21-УМ «О внесении изменения в указ Мэра Москвы от 5 марта 2020 г. № 12-УМ»')
-}
-covid()
