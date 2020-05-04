11d12891060807f2a99ff44980ff809f15e4769e
library129 <57745774+library129@users.noreply.github.com>
2020-05-04 19:03:50 +0300

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,43 +1,43 @@
-(function() {
-		const headings = document.querySelectorAll('h2.collapsible-headline');
-		Array.prototype.forEach.call(headings, heading => {
-			let btn = heading.querySelector('button');
-			let target = heading.nextElementSibling;
-			btn.onclick = () => {
-				let expanded =
-					btn.getAttribute('aria-expanded') === 'true' || false;
-				btn.setAttribute('aria-expanded', !expanded);
-				target.hidden = expanded;
-			}
-		})
-	})()
 
-let searchInput = document.querySelector('input');
-let searchButton = document.querySelector('.search__btn');
+"use strict";
 
-let searchResults = document.querySelector('#searchResults')
+(function () {
+  var headings = document.querySelectorAll('h2.collapsible-headline');
+  Array.prototype.forEach.call(headings, function (heading) {
+    var btn = heading.querySelector('button');
+    var target = heading.nextElementSibling;
+
+    btn.onclick = function () {
+      var expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+      btn.setAttribute('aria-expanded', !expanded);
+      target.hidden = expanded;
+    };
+  });
+})();
+
+var searchInput = document.querySelector('input');
+var searchButton = document.querySelector('.search__btn');
+var searchResults = document.querySelector('#searchResults');
 
 function searchBooks() {
-	fetch("./resources/catalog/catalog.json")
-        .then((response) => {
-            return response.json()
-        })
-        .then((data) => {
-            let result = (data['books'].join().toLowerCase().includes(searchInput.value.toLowerCase().trim()))
-			searchResults.textContent = '';
-			if (result) {
-				searchResults.textContent = 'Книга найдена в фонде библиотеки. Для бронирования книги позвоните или напишите нам';
-				searchInput.focus();
-            } else {
-                searchResults.textContent = 'Книга не найдена';
-            }
-        })
-        .catch((e) => {
-            console.log(e)
-		})
+  fetch("./resources/catalog/catalog.json").then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    var result = data['books'].join().toLowerCase().includes(searchInput.value.toLowerCase().trim());
+    searchResults.textContent = '';
+
+    if (result) {
+      searchResults.textContent = 'Книга найдена в фонде библиотеки. Для бронирования книги позвоните или напишите нам';
+      searchInput.focus();
+    } else {
+      searchResults.textContent = 'Книга не найдена';
+    }
+  })["catch"](function (e) {
+    console.log(e);
+  });
 }
 
-searchButton.addEventListener('click', (event) => {
-	event.preventDefault();
-	searchBooks();
-})
+searchButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  searchBooks();
+});
